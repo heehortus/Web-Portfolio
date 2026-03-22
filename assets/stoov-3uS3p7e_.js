d625d654e8277808b811cd295624ed73edefd3d6
heehortus <hee.hortus@gmail.com>
2026-03-22 21:58:04 +0900

    Updates

---
+++ assets/stoov-3uS3p7e_.js
@@ -0,0 +1,64 @@
+import{j as s}from"./index-C6fyYbzD.js";const o="/Web-Portfolio/assets/stoov-1-CZcrTs2b.png",t="/Web-Portfolio/assets/stoov-2-Dd3o-pQ5.png",h="/Web-Portfolio/assets/stoov-3-ak5f4zgk.png",i="/Web-Portfolio/assets/stoov-4-D5aFIZRx.png",c="/Web-Portfolio/assets/stoov-5-Bb8yEHqp.png",x="/Web-Portfolio/assets/stoov-6-id6aNcNE.png",j="/Web-Portfolio/assets/stoov-7-BTnYtLA1.png",l="/Web-Portfolio/assets/stoov-8-DRNhbu7L.png",d="/Web-Portfolio/assets/stoov-9-E6wDYTHL.png";function e(r){const n={h2:"h2",h3:"h3",hr:"hr",p:"p",...r.components};return s.jsxs(s.Fragment,{children:[s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{children:"Overview"}),`
+`,s.jsx(n.h3,{children:"버스킹 공연을 하기 위한 규제와 절차가 늘어나고 있어요."}),`
+`,s.jsx("img",{src:o,alt:"STOOV OVERVIEW"}),`
+`,s.jsxs(n.p,{children:["아이데이션을 통해 ",s.jsx("strong",{children:"음악"}),"이라는 공통 관심 주제에서 시작하여 관객의 현장감과 참여도가 높은 ",s.jsx("strong",{children:"버스킹 공연"}),`으로 아이디어를 확정했습니다.\r
+이후 버스킹 공연 시장을 조사하고, 이해 관계자를 대상으로 설문조사 및 인터뷰를 진행하여 문제 상황에 대해 파악했습니다.`]}),`
+`,s.jsx(n.p,{children:`버스킹 공연을 하기 위해서는 공연 장소를 확보해야 합니다.\r
+그러나 공연 장소는 대부분 공공장소이기 때문에 규제와 사전 절차가 많습니다. 또한 대다수의 공연 장소가 전국에 흩어져 있고, 이를 관리하는 기관도 다양하기 때문에 공연자들은 항상 하던 장소에서만 공연하는 매너리즘 역시 겪고 있습니다.`}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{children:"Problem Statement"}),`
+`,s.jsx(n.h3,{children:"빠르게 버스킹 공연 장소에 대해 정보를 탐색할 수 있다면?"}),`
+`,s.jsx("img",{src:t,alt:"STOOV PROBLEM STATEMENT"}),`
+`,s.jsxs(n.p,{children:[`버스킹 공연자를 대상으로 심층 인터뷰를 진행하여 이들의 페인 포인트를 더 자세하게 조사했습니다.\r
+`,s.jsx("br",{}),"설문조사 및 인터뷰 인사이트를 바탕으로 ",s.jsx("strong",{children:"빠르게 버스킹 공연 장소에 대해 탐색할 수 있다면?"}),` 이라는 가설을 세우고,\r
+이를 검증하고자 서비스 목표를 정의했습니다.`]}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{children:"Service Goal"}),`
+`,s.jsx(n.h3,{children:"버스커가 더 쉽게 공연 장소를 탐색하고 결정할 수 있도록 돕는 서비스"}),`
+`,s.jsx("img",{src:h,alt:"STOOV SERVICE GOAL"}),`
+`,s.jsxs(n.p,{children:["공연할 수 있는 장소는 어디에나 있습니다. ",s.jsx("br",{}),`\r
+그러나 공연자는 여전히 어느 지자체에서, 언제 예약할 수 있는지, 장소에 어떤 제한이 있는지 바로 알기 어렵습니다. `,s.jsx("br",{}),`\r
+이에 팀은 장소를 `,s.jsx("strong",{children:"한눈에 모아볼 수 있는 지도"}),"를 채택하여 ",s.jsx("strong",{children:"버스킹 공연 장소에 대한 정보와 후기"}),"를 빠르게 볼 수 있는 서비스 MVP를 설정했습니다."]}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{children:"Service Features"}),`
+`,s.jsx(n.h3,{children:"1. 홈 / 서울시 공연 장소 지도"}),`
+`,s.jsx("img",{src:i,alt:"STOOV SERVICE FEATURES 1"}),`
+`,s.jsxs(n.p,{children:[s.jsx("strong",{children:"서울시의 모든 버스킹 공연 장소, 지도로 빠르게 탐색할 수 있어요."}),s.jsx("br",{}),`\r
+서울시 공공기반의 공식 공연 가능 장소를 노출해요.\r
+자유롭게 지도를 탐색하여 서울시의 공연 장소를 확인해요.`]}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h3,{children:"2. 취향에 맞는 장소 필터링"}),`
+`,s.jsx("img",{src:c,alt:"STOOV SERVICE FEATURES 2"}),`
+`,s.jsxs(n.p,{children:[s.jsx("strong",{children:"필터링 태그로 내가 선호하는 공연 장소만 찾을 수 있어요."}),s.jsx("br",{}),`\r
+서울시 공연 장소 유형에 따라 정한 세 가지 타입의 필터링 태그를 사용할 수 있어요.`]}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h3,{children:"3. 빠른 정보 탐색"}),`
+`,s.jsx("img",{src:x,alt:"STOOV SERVICE FEATURES 3"}),`
+`,s.jsxs(n.p,{children:[s.jsx("strong",{children:"화면을 벗어나지 않고 빠르게, 공연 장소에 대한 정보를 탐색할 수 있어요."}),s.jsx("br",{}),`\r
+여러 번의 탭을 거치지 않고 스와이프 한 번으로 장소 정보를 확인할 수 있어요.`]}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h3,{children:"4. 공연 장소 후기"}),`
+`,s.jsx("img",{src:j,alt:"STOOV SERVICE FEATURES 4"}),`
+`,s.jsxs(n.p,{children:[s.jsx("strong",{children:"공연 경험을 모두와 공유해요."}),s.jsx("br",{}),`\r
+직접 공연 후 경험을 다른 버스커들에게 공유하고, 버스커들의 후기를 통해 더욱 생생한 현장 분위기를 파악해요.`]}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{children:"User Test"}),`
+`,s.jsx(n.h3,{children:"사용자 테스트를 통해 서비스의 유용성과 사용성을 검증했어요."}),`
+`,s.jsx("img",{src:l,alt:"STOOV USER TEST"}),`
+`,s.jsxs(n.p,{children:[s.jsx("strong",{children:"사용자들은 공연 장소 정보의 통합 자체를 큰 가치로 인식했어요."}),s.jsx("br",{}),`\r
+UT 결과, 기존의 파편화된 장소 탐색/예약 경험을 대체할 수 있는 서비스로서의 유용성 및 사용 의향을 검증했어요.`,s.jsx("br",{})]}),`
+`,s.jsx(n.p,{children:"(응답자: 수도권 버스커 6명, MAZE, 서비스 자유 탐색 후 인터뷰)"}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{children:"Project Review"}),`
+`,s.jsx(n.h3,{children:"서비스 구축과 디자인 시스템 운영,협업을 통해 얻은 실무 경험"}),`
+`,s.jsx("img",{src:d,alt:"STOOV PROJECT REVIEW"}),`
+`,s.jsx(n.p,{children:`서비스 MVP를 정할 때부터 ‘어떤 도메인을 정해야 할까?’부터 시작해 ‘5주 동안 어디까지 구현할 수 있을까?’ 등 많은 고민이 있었습니다.\r
+여러 번의 아이디어 회의를 거쳐 제가 제시한 ‘버스킹 공연’이라는 아이디어가 채택되어 프로젝트를 점점 구체화할 수 있었습니다.`}),`
+`,s.jsxs(n.p,{children:["지도형 플랫폼을 선택하면서 API의 한계로 개발 가능한 구조가 한정되기도 했지만, 그 안에서 최대한 사용자가 편한 구조가 무엇일까, 열심히 고민하고 설계했습니다. ",s.jsx("strong",{children:"디자인 시스템 운영, 개발자 핸드 오프, 디자인 QA"}),"까지 거쳐 서비스를 성공적으로 배포할 수 있었던 귀중한 경험이었습니다."]}),`
+`,s.jsx(n.p,{children:`특히 이해도나 언어가 다른 부서끼리 소통하기 쉽도록 주도적으로 문서를 정리하여 날마다 진행하는 데일리 스크럼이 원활히 진행될 수 있었습니다.\r
+앞으로의 협업 경험에 도움이 될 소통 방식을 깨우칠 수 있었던 프로젝트였습니다.`}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{children:"Site"}),`
+`,s.jsx("a",{href:"https://happy-dandelion-8e9.notion.site/STOOV-30687b16b50080809a9ac93314571add?pvs=143",target:"_blank",rel:"noopener noreferrer",children:"🔗팀 노션"}),`
+`,s.jsx("a",{href:"https://www.figma.com/community/file/1606330306165462807/stoov-design-system-library",target:"_blank",rel:"noopener noreferrer",children:"🔗디자인 시스템"})]})}function g(r={}){const{wrapper:n}=r.components||{};return n?s.jsx(n,{...r,children:s.jsx(e,{...r})}):e(r)}export{g as default};
